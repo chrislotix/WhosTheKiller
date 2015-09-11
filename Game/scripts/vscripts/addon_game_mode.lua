d71c89b7b8baa129337f1d7dde5b171f8f67a604
Pizzalol <siprak214@hotmail.com>
2015-09-11 14:38:53 +0200

    Update file structure to Reborn standards

--- Game/scripts/vscripts/addon_game_mode.lua
+++ Game/scripts/vscripts/addon_game_mode.lua
@@ -1,99 +1,48 @@
-BASE_MODULES = {
-	'util',
-	'timers',
-	'physics',
-	'lib.statcollection',
-	'abilities',
-	'killer',
-	'multiteam',
-	'events',
-	'consolecommands',
-}
+-- This is the entry-point to your game mode and should be used primarily to precache models/particles/sounds/etc
+
+require('internal/util')
+require('narrowmaze')
 
 function Precache( context )
-	-- NOTE: IT IS RECOMMENDED TO USE A MINIMAL AMOUNT OF LUA PRECACHING, AND A MAXIMAL AMOUNT OF DATADRIVEN PRECACHING.
-	-- Precaching guide: https://moddota.com/forums/discussion/119/precache-fixing-and-avoiding-issues
+--[[
+  This function is used to precache resources/units/items/abilities that will be needed
+  for sure in your game and that will not be precached by hero selection.  When a hero
+  is selected from the hero selection screen, the game will precache that hero's assets,
+  any equipped cosmetics, and perform the data-driven precaching defined in that hero's
+  precache{} block, as well as the precache{} block for any equipped abilities.
 
-	print("[KILLER] Performing pre-load precache")
+  See NarrowMaze:PostLoadPrecache() in narrowmaze.lua for more information
+  ]]
 
-	-- Particles can be precached individually or by folder
-	-- It it likely that precaching a single particle system will precache all of its children, but this may not be guaranteed
-	--PrecacheResource("particle", "particles/econ/generic/generic_aoe_explosion_sphere_1/generic_aoe_explosion_sphere_1.vpcf", context)
-	--PrecacheResource("particle_folder", "particles/test_particle", context)
+  DebugPrint("[NARROWMAZE] Performing pre-load precache")
 
-	-- Models can also be precached by folder or individually
-	--PrecacheModel should generally used over PrecacheResource for individual models
-	--PrecacheResource("model_folder", "particles/heroes/antimage", context)
-	--PrecacheResource("model", "particles/heroes/viper/viper.vmdl", context)
-	--PrecacheModel("models/heroes/viper/viper.vmdl", context)
+  -- Particles can be precached individually or by folder
+  -- It it likely that precaching a single particle system will precache all of its children, but this may not be guaranteed
+  PrecacheResource("particle", "particles/econ/generic/generic_aoe_explosion_sphere_1/generic_aoe_explosion_sphere_1.vpcf", context)
+  PrecacheResource("particle_folder", "particles/test_particle", context)
 
-	-- Sounds can precached here like anything else
-	PrecacheResource("soundfile", "soundevents/game_sounds_heroes/game_sounds_abaddon.vsndevts", context)
-	PrecacheResource("soundfile", "soundevents/game_sounds_heroes/game_sounds_beastmaster.vsndevts", context)
-	PrecacheResource("soundfile", "soundevents/custom_sounds.vsndevts", context)
-	PrecacheResource("soundfile", "soundevents/spook_sounds.vsndevts", context)
-	PrecacheResource("particle", "particles/rain_fx/econ_rain.vpcf", context)
+  -- Models can also be precached by folder or individually
+  -- PrecacheModel should generally used over PrecacheResource for individual models
+  PrecacheResource("model_folder", "particles/heroes/antimage", context)
+  PrecacheResource("model", "particles/heroes/viper/viper.vmdl", context)
+  PrecacheModel("models/heroes/viper/viper.vmdl", context)
 
-	-- Entire items can be precached by name
-	-- Abilities can also be precached in this way despite the name
-	PrecacheItemByNameSync("example_ability", context)
-	PrecacheItemByNameSync("item_example_item", context)
+  -- Sounds can precached here like anything else
+  PrecacheResource("soundfile", "soundevents/game_sounds_heroes/game_sounds_gyrocopter.vsndevts", context)
 
-	-- Entire heroes (sound effects/voice/models/particles) can be precached with PrecacheUnitByNameSync
-	-- Custom units from npc_units_custom.txt can also have all of their abilities and precache{} blocks precached in this way
-	PrecacheUnitByNameSync("killer", context)
+  -- Entire items can be precached by name
+  -- Abilities can also be precached in this way despite the name
+  PrecacheItemByNameSync("example_ability", context)
+  PrecacheItemByNameSync("item_example_item", context)
+
+  -- Entire heroes (sound effects/voice/models/particles) can be precached with PrecacheUnitByNameSync
+  -- Custom units from npc_units_custom.txt can also have all of their abilities and precache{} blocks precached in this way
+  PrecacheUnitByNameSync("npc_dota_hero_ancient_apparition", context)
+  PrecacheUnitByNameSync("npc_dota_hero_enigma", context)
 end
-
---MODULE LOADER STUFF by Adynathos
-BASE_LOG_PREFIX = '[]'
-
-
-LOG_FILE = "log/killer.txt"
-
-InitLogFile(LOG_FILE, "[[ killer ]]")
-
-function log(msg)
-	print(BASE_LOG_PREFIX .. msg)
-	AppendToLogFile(LOG_FILE, msg .. '\n')
-end
-
-function err(msg)
-	display('[X] '..msg, COLOR_RED)
-end
-
-function warning(msg)
-	display('[W] '..msg, COLOR_DYELLOW)
-end
-
-function display(text, color)
-	color = color or COLOR_LGREEN
-
-	log('> '..text)
-
-	Say(nil, color..text, false)
-end
-
-local function load_module(mod_name)
-	-- load the module in a monitored environment
-	local status, err_msg = pcall(function()
-		require(mod_name)
-	end)
-
-	if status then
-		log(' module ' .. mod_name .. ' OK')
-	else
-		err(' module ' .. mod_name .. ' FAILED: '..err_msg)
-	end
-end
-
--- Load all modules
-for i, mod_name in pairs(BASE_MODULES) do
-	load_module(mod_name)
-end
---END OF MODULE LOADER STUFF
 
 -- Create the game mode when we activate
 function Activate()
-	GameRules.killer = killer()
-	GameRules.killer:Initkiller()
+  GameRules.NarrowMaze = NarrowMaze()
+  GameRules.NarrowMaze:InitNarrowMaze()
 end
--- Game/scripts/vscripts/events.lua
+++ Game/scripts/vscripts/events.lua
@@ -1,269 +1,334 @@
---[[
-  This function is called once and only once after all players have loaded into the game, right as the hero selection time begins.
-  It can be used to initialize non-hero player state or adjust the hero selection (i.e. force random etc)
-]]
-function killer:OnAllPlayersLoaded()
-	print("[KILLER] All Players have loaded into the game")
+-- This file contains all narrowmaze-registered events and has already set up the passed-in parameters for your use.
+-- Do not remove the NarrowMaze:_Function calls in these events as it will mess with the internal narrowmaze systems.
 
-	-- Command for model combining
-	ConsoleCommands:SendToAll("dota_combine_models 0")
+-- Cleanup a player when they leave
+function NarrowMaze:OnDisconnect(keys)
+  DebugPrint('[NARROWMAZE] Player Disconnected ' .. tostring(keys.userid))
+  DebugPrintTable(keys)
 
-	-- Command to stop all sounds
-	SendToConsole("stopsound")
-	SendToServerConsole("stopsound")
-
-	-- Background music
-	Timers:CreateTimer(function()
-		EmitGlobalSound("Killer.Background")
-		return 128
-	end)
-
-	self.player_table = {}
-	local counter = 0
-
-	for nPlayerID = 0, (DOTA_MAX_TEAM_PLAYERS-1) do
-		local player_handle = PlayerResource:GetPlayer(nPlayerID)
-		if player_handle then
-			self.player_table[counter] = player_handle
-			player_handle.voted = false
-			counter = counter + 1
-		end
-	end
-
-	self.num_players = counter
-
-
-	-- Random the killer here
-	local player = RandomInt(0, counter - 1)
-	print("# User IDs: " .. counter)
-	print("Randomed player ID: " .. player)
-
-	self.player_table[player].killer = true
-
-	-- set game_started to true after a couple of seconds
-
-	Timers:CreateTimer(25,function()
-		self.game_started = true
-	end)
+  local name = keys.name
+  local networkid = keys.networkid
+  local reason = keys.reason
+  local userid = keys.userid
 
 end
+-- The overall game state has changed
+function NarrowMaze:OnGameRulesStateChange(keys)
+  DebugPrint("[NARROWMAZE] GameRules State Changed")
+  DebugPrintTable(keys)
 
---[[
-  This function is called once and only once for every player when they spawn into the game for the first time.  It is also called
-  if the player's hero is replaced with a new hero for any reason.  This function is useful for initializing heroes, such as adding
-  levels, changing the starting gold, removing/adding abilities, adding physics, etc.
+  -- This internal handling is used to set up main narrowmaze functions
+  NarrowMaze:_OnGameRulesStateChange(keys)
 
-  The hero parameter is the hero entity that just spawned in.
-]]
-function killer:OnHeroInGame(hero)
-	print("[KILLER] Hero spawned in game for first time -- " .. hero:GetUnitName())
-	local player_id = hero:GetPlayerID()
-	print("Hero Self " .. tostring(self))
-
-	if not self.greetPlayers then
-		-- At this point a player now has a hero spawned in your map.
-		
-	    local firstLine = ColorIt("Welcome to ", "green") .. ColorIt("Escape from the Narrow Maze! ", "magenta") .. ColorIt("v1.0", "blue");
-	    local secondLine = ColorIt("Developers: ", "green") .. ColorIt("Pizzalol & chrislotix", "orange")
-		-- Send the first greeting in 4 secs.
-		Timers:CreateTimer(4, function()
-	        GameRules:SendCustomMessage(firstLine, 0, 0)
-	        GameRules:SendCustomMessage(secondLine, 0, 0)
-		end)
-
-		self.greetPlayers = true
-	end
-
-	-- Store a reference to the player handle inside this hero handle.
-	hero.player = PlayerResource:GetPlayer(player_id)
-	-- Store the player's name inside this hero handle.
-	hero.playerName = PlayerResource:GetPlayerName(player_id)
-	-- Store this hero handle in this table.
-	table.insert(self.vPlayers, hero)
-
-	if Testing then
-		--Say(nil, "Testing is on.", false)
-	end
-
-	print("HELLO TESTING ABILITIES")
-	InitAbilities(hero)
-
-	ParticleManager:CreateParticle("particles/rain_fx/econ_rain.vpcf", PATTACH_EYES_FOLLOW, hero)
-	EmitGlobalSound("sounds/ambient/soundscapes/light_rain_lp.vsnd")
-
-	hero:AddNewModifier(hero, nil, "modifier_stunned", {})
-
-	local ability = hero:GetAbilityByIndex(0)
-
-	ability:ApplyDataDrivenModifier(hero, hero, "modifier_killer_attack", {})
-	ability:ApplyDataDrivenModifier(hero, hero, "modifier_peasant_disarm", {})
+  local newState = GameRules:State_Get()
 end
 
 -- An NPC has spawned somewhere in game.  This includes heroes
-function killer:OnNPCSpawned(keys)
-	--print("[KILLER] NPC Spawned")
-	--PrintTable(keys)
-	local npc = EntIndexToHScript(keys.entindex)
+function NarrowMaze:OnNPCSpawned(keys)
+  DebugPrint("[NARROWMAZE] NPC Spawned")
+  DebugPrintTable(keys)
 
-	if npc:IsRealHero() and npc.bFirstSpawned == nil then
-		npc.bFirstSpawned = true
-		killer:OnHeroInGame(npc)
-	end
-end
+  -- This internal handling is used to set up main narrowmaze functions
+  NarrowMaze:_OnNPCSpawned(keys)
 
--- This function is called once when the player fully connects and becomes "Ready" during Loading
-function killer:OnConnectFull(keys)
-	print ('[KILLER] OnConnectFull')
-	--PrintTable(keys)
-	killer:Capturekiller()
-
-	local entIndex = keys.index+1
-	-- The Player entity of the joining user
-	local ply = EntIndexToHScript(entIndex)
-
-	-- The Player ID of the joining player
-	local playerID = ply:GetPlayerID()
-
-	-- Update the user ID table with this user
-	--if ply then
-		self.vUserIds[keys.userid] = ply
-	--end
-
-	-- Update the Steam ID table
-	self.vSteamIds[PlayerResource:GetSteamAccountID(playerID)] = ply
-
-	-- If the player is a broadcaster flag it in the Broadcasters table
-	if PlayerResource:IsBroadcaster(playerID) then
-		self.vBroadcasters[keys.userid] = 1
-		return
-	end
-end
-
----------------------------------------------------------------------------
--- Game state change handler
----------------------------------------------------------------------------
-function killer:OnGameRulesStateChange()
-	local nNewState = GameRules:State_Get()
-	print( "OnGameRulesStateChange: " .. nNewState )
-
-	if nNewState == DOTA_GAMERULES_STATE_PRE_GAME then
-		--ShowGenericPopup( "#multiteam_instructions_title", "#multiteam_instructions_body", tostring(self.TEAM_KILLS_TO_WIN), "", DOTA_SHOWGENERICPOPUP_TINT_SCREEN )
-	end
-
-	if nNewState == DOTA_GAMERULES_STATE_HERO_SELECTION then
-		GameRules:GetGameModeEntity():SetThink( "EnsurePlayersOnCorrectTeam", self, 0 )
-		GameRules:GetGameModeEntity():SetThink( "BroadcastPlayerTeamAssignments", self, 1 )
-		ShowGenericPopup( "#multiteam_instructions_title", "#multiteam_instructions_body", tostring(self.TEAM_KILLS_TO_WIN), "", DOTA_SHOWGENERICPOPUP_TINT_SCREEN )
-
-		local et = 1
-		if self.bSeenWaitForPlayers then
-			et = .01
-		end
-		Timers:CreateTimer("alljointimer", {
-			useGameTime = true,
-			endTime = et,
-			callback = function()
-				if PlayerResource:HaveAllPlayersJoined() then
-					killer:PostLoadPrecache()
-					killer:OnAllPlayersLoaded()
-					return
-				end
-				return 1
-			end})
-	end
-
-	if nNewState == DOTA_GAMERULES_STATE_GAME_IN_PROGRESS then
-		GameRules:GetGameModeEntity():SetThink( "OnThink", self, 1 ) 
-	end
-
-	if nNewState >= DOTA_GAMERULES_STATE_POST_GAME then
-		statcollection.sendStats({})
-	end
-end
-
-function killer:PostLoadPrecache()
-	--print("[KILLER] Performing Post-Load precache")
-
-	PrecacheUnitByNameAsync("npc_precache_everything", function(...) end)
-end
-
---[[
-  This function is called once and only once as soon as the first player (almost certain to be the server in local lobbies) loads in.
-  It can be used to initialize state that isn't initializeable in Initkiller() but needs to be done before everyone loads in.
-]]
-function killer:OnFirstPlayerLoaded()
-	--print("[KILLER] First Player has loaded")
-end
-
--- Cleanup a player when they leave
-function killer:OnDisconnect(keys)
-	--print('[KILLER] Player Disconnected ' .. tostring(keys.userid))
-	--PrintTable(keys)
-
-	local name = keys.name
-	local networkid = keys.networkid
-	local reason = keys.reason
-	local userid = keys.userid
-end
-
--- A player has reconnected to the game.  This function can be used to repaint Player-based particles or change
--- state as necessary
-function killer:OnPlayerReconnect(keys)
-	--print ( '[KILLER] OnPlayerReconnect' )
-	--PrintTable(keys)
+  local npc = EntIndexToHScript(keys.entindex)
 end
 
 -- An entity somewhere has been hurt.  This event fires very often with many units so don't do too many expensive
 -- operations here
-function killer:OnEntityHurt(keys)
-	--print("[KILLER] Entity Hurt")
-	--PrintTable(keys)
-	local attacker = EntIndexToHScript(keys.entindex_attacker)
-	local victim = EntIndexToHScript(keys.entindex_killed)
+function NarrowMaze:OnEntityHurt(keys)
+  --DebugPrint("[NARROWMAZE] Entity Hurt")
+  --DebugPrintTable(keys)
+
+  local damagebits = keys.damagebits -- This might always be 0 and therefore useless
+  if keys.entindex_attacker ~= nil and keys.entindex_killed ~= nil then
+    local entCause = EntIndexToHScript(keys.entindex_attacker)
+    local entVictim = EntIndexToHScript(keys.entindex_killed)
+
+    -- The ability/item used to damage, or nil if not damaged by an item/ability
+    local damagingAbility = nil
+
+    if keys.entindex_inflictor ~= nil then
+      damagingAbility = EntIndexToHScript( keys.entindex_inflictor )
+    end
+  end
+end
+
+-- An item was picked up off the ground
+function NarrowMaze:OnItemPickedUp(keys)
+  DebugPrint( '[NARROWMAZE] OnItemPickedUp' )
+  DebugPrintTable(keys)
+
+  local heroEntity = EntIndexToHScript(keys.HeroEntityIndex)
+  local itemEntity = EntIndexToHScript(keys.ItemEntityIndex)
+  local player = PlayerResource:GetPlayer(keys.PlayerID)
+  local itemname = keys.itemname
+end
+
+-- A player has reconnected to the game.  This function can be used to repaint Player-based particles or change
+-- state as necessary
+function NarrowMaze:OnPlayerReconnect(keys)
+  DebugPrint( '[NARROWMAZE] OnPlayerReconnect' )
+  DebugPrintTable(keys) 
+end
+
+-- An item was purchased by a player
+function NarrowMaze:OnItemPurchased( keys )
+  DebugPrint( '[NARROWMAZE] OnItemPurchased' )
+  DebugPrintTable(keys)
+
+  -- The playerID of the hero who is buying something
+  local plyID = keys.PlayerID
+  if not plyID then return end
+
+  -- The name of the item purchased
+  local itemName = keys.itemname 
+  
+  -- The cost of the item purchased
+  local itemcost = keys.itemcost
+  
+end
+
+-- An ability was used by a player
+function NarrowMaze:OnAbilityUsed(keys)
+  DebugPrint('[NARROWMAZE] AbilityUsed')
+  DebugPrintTable(keys)
+
+  local player = PlayerResource:GetPlayer(keys.PlayerID)
+  local abilityname = keys.abilityname
+end
+
+-- A non-player entity (necro-book, chen creep, etc) used an ability
+function NarrowMaze:OnNonPlayerUsedAbility(keys)
+  DebugPrint('[NARROWMAZE] OnNonPlayerUsedAbility')
+  DebugPrintTable(keys)
+
+  local abilityname=  keys.abilityname
+end
+
+-- A player changed their name
+function NarrowMaze:OnPlayerChangedName(keys)
+  DebugPrint('[NARROWMAZE] OnPlayerChangedName')
+  DebugPrintTable(keys)
+
+  local newName = keys.newname
+  local oldName = keys.oldName
+end
+
+-- A player leveled up an ability
+function NarrowMaze:OnPlayerLearnedAbility( keys)
+  DebugPrint('[NARROWMAZE] OnPlayerLearnedAbility')
+  DebugPrintTable(keys)
+
+  local player = EntIndexToHScript(keys.player)
+  local abilityname = keys.abilityname
+end
+
+-- A channelled ability finished by either completing or being interrupted
+function NarrowMaze:OnAbilityChannelFinished(keys)
+  DebugPrint('[NARROWMAZE] OnAbilityChannelFinished')
+  DebugPrintTable(keys)
+
+  local abilityname = keys.abilityname
+  local interrupted = keys.interrupted == 1
+end
+
+-- A player leveled up
+function NarrowMaze:OnPlayerLevelUp(keys)
+  DebugPrint('[NARROWMAZE] OnPlayerLevelUp')
+  DebugPrintTable(keys)
+
+  local player = EntIndexToHScript(keys.player)
+  local level = keys.level
+end
+
+-- A player last hit a creep, a tower, or a hero
+function NarrowMaze:OnLastHit(keys)
+  DebugPrint('[NARROWMAZE] OnLastHit')
+  DebugPrintTable(keys)
+
+  local isFirstBlood = keys.FirstBlood == 1
+  local isHeroKill = keys.HeroKill == 1
+  local isTowerKill = keys.TowerKill == 1
+  local player = PlayerResource:GetPlayer(keys.PlayerID)
+  local killedEnt = EntIndexToHScript(keys.EntKilled)
+end
+
+-- A tree was cut down by tango, quelling blade, etc
+function NarrowMaze:OnTreeCut(keys)
+  DebugPrint('[NARROWMAZE] OnTreeCut')
+  DebugPrintTable(keys)
+
+  local treeX = keys.tree_x
+  local treeY = keys.tree_y
+end
+
+-- A rune was activated by a player
+function NarrowMaze:OnRuneActivated (keys)
+  DebugPrint('[NARROWMAZE] OnRuneActivated')
+  DebugPrintTable(keys)
+
+  local player = PlayerResource:GetPlayer(keys.PlayerID)
+  local rune = keys.rune
+
+  --[[ Rune Can be one of the following types
+  DOTA_RUNE_DOUBLEDAMAGE
+  DOTA_RUNE_HASTE
+  DOTA_RUNE_HAUNTED
+  DOTA_RUNE_ILLUSION
+  DOTA_RUNE_INVISIBILITY
+  DOTA_RUNE_BOUNTY
+  DOTA_RUNE_MYSTERY
+  DOTA_RUNE_RAPIER
+  DOTA_RUNE_REGENERATION
+  DOTA_RUNE_SPOOKY
+  DOTA_RUNE_TURBO
+  ]]
+end
+
+-- A player took damage from a tower
+function NarrowMaze:OnPlayerTakeTowerDamage(keys)
+  DebugPrint('[NARROWMAZE] OnPlayerTakeTowerDamage')
+  DebugPrintTable(keys)
+
+  local player = PlayerResource:GetPlayer(keys.PlayerID)
+  local damage = keys.damage
 end
 
 -- A player picked a hero
-function killer:OnPlayerPickHero(keys)
-	--print ('[KILLER] OnPlayerPickHero')
-	--PrintTable(keys)
+function NarrowMaze:OnPlayerPickHero(keys)
+  DebugPrint('[NARROWMAZE] OnPlayerPickHero')
+  DebugPrintTable(keys)
 
-	local heroClass = keys.hero
-	local heroEntity = EntIndexToHScript(keys.heroindex)
-	local player = EntIndexToHScript(keys.player)
+  local heroClass = keys.hero
+  local heroEntity = EntIndexToHScript(keys.heroindex)
+  local player = EntIndexToHScript(keys.player)
+end
+
+-- A player killed another player in a multi-team context
+function NarrowMaze:OnTeamKillCredit(keys)
+  DebugPrint('[NARROWMAZE] OnTeamKillCredit')
+  DebugPrintTable(keys)
+
+  local killerPlayer = PlayerResource:GetPlayer(keys.killer_userid)
+  local victimPlayer = PlayerResource:GetPlayer(keys.victim_userid)
+  local numKills = keys.herokills
+  local killerTeamNumber = keys.teamnumber
 end
 
 -- An entity died
-function killer:OnEntityKilled( keys )
-	print( '[KILLER] OnEntityKilled Called' )
-	--PrintTable( keys )
+function NarrowMaze:OnEntityKilled( keys )
+  DebugPrint( '[NARROWMAZE] OnEntityKilled Called' )
+  DebugPrintTable( keys )
 
-	-- The Unit that was Killed
-	local killedUnit = EntIndexToHScript( keys.entindex_killed )
-	-- The Killing entity
-	local killerEntity = nil
+  NarrowMaze:_OnEntityKilled( keys )
+  
 
-	if keys.entindex_attacker ~= nil then
-		killerEntity = EntIndexToHScript( keys.entindex_attacker )
-	end
+  -- The Unit that was Killed
+  local killedUnit = EntIndexToHScript( keys.entindex_killed )
+  -- The Killing entity
+  local killerEntity = nil
 
-	if killedUnit:IsRealHero() then
-		--print ("KILLEDKILLER: " .. killedUnit:GetName() .. " -- " .. killerEntity:GetName())
-		if not killedUnit.death_counter then
-			killedUnit.death_counter = 1
-		else
-			killedUnit.death_counter = killedUnit.death_counter + 1
-		end
+  if keys.entindex_attacker ~= nil then
+    killerEntity = EntIndexToHScript( keys.entindex_attacker )
+  end
 
-		-- Disable respawn of the player
-		if killedUnit.death_counter >= 3 or self.phase_three_init then
-			Timers:CreateTimer(3.1 ,function()
-				print("Testing stoning")
+  -- The ability/item used to kill, or nil if not killed by an item/ability
+  local killerAbility = nil
 
-				local ability = killedUnit:GetAbilityByIndex(0)
-				print(killedUnit:GetUnitName())
+  if keys.entindex_inflictor ~= nil then
+    killerAbility = EntIndexToHScript( keys.entindex_inflictor )
+  end
 
-				ability:ApplyDataDrivenModifier(killedUnit, killedUnit, "modifier_killer_vote_dead", {})
-			end)
-		end
-	end
+  local damagebits = keys.damagebits -- This might always be 0 and therefore useless
+
+  -- Put code here to handle when an entity gets killed
+end
+
+
+
+-- This function is called 1 to 2 times as the player connects initially but before they 
+-- have completely connected
+function NarrowMaze:PlayerConnect(keys)
+  DebugPrint('[NARROWMAZE] PlayerConnect')
+  DebugPrintTable(keys)
+end
+
+-- This function is called once when the player fully connects and becomes "Ready" during Loading
+function NarrowMaze:OnConnectFull(keys)
+  DebugPrint('[NARROWMAZE] OnConnectFull')
+  DebugPrintTable(keys)
+
+  NarrowMaze:_OnConnectFull(keys)
+  
+  local entIndex = keys.index+1
+  -- The Player entity of the joining user
+  local ply = EntIndexToHScript(entIndex)
+  
+  -- The Player ID of the joining player
+  local playerID = ply:GetPlayerID()
+end
+
+-- This function is called whenever illusions are created and tells you which was/is the original entity
+function NarrowMaze:OnIllusionsCreated(keys)
+  DebugPrint('[NARROWMAZE] OnIllusionsCreated')
+  DebugPrintTable(keys)
+
+  local originalEntity = EntIndexToHScript(keys.original_entindex)
+end
+
+-- This function is called whenever an item is combined to create a new item
+function NarrowMaze:OnItemCombined(keys)
+  DebugPrint('[NARROWMAZE] OnItemCombined')
+  DebugPrintTable(keys)
+
+  -- The playerID of the hero who is buying something
+  local plyID = keys.PlayerID
+  if not plyID then return end
+  local player = PlayerResource:GetPlayer(plyID)
+
+  -- The name of the item purchased
+  local itemName = keys.itemname 
+  
+  -- The cost of the item purchased
+  local itemcost = keys.itemcost
+end
+
+-- This function is called whenever an ability begins its PhaseStart phase (but before it is actually cast)
+function NarrowMaze:OnAbilityCastBegins(keys)
+  DebugPrint('[NARROWMAZE] OnAbilityCastBegins')
+  DebugPrintTable(keys)
+
+  local player = PlayerResource:GetPlayer(keys.PlayerID)
+  local abilityName = keys.abilityname
+end
+
+-- This function is called whenever a tower is killed
+function NarrowMaze:OnTowerKill(keys)
+  DebugPrint('[NARROWMAZE] OnTowerKill')
+  DebugPrintTable(keys)
+
+  local gold = keys.gold
+  local killerPlayer = PlayerResource:GetPlayer(keys.killer_userid)
+  local team = keys.teamnumber
+end
+
+-- This function is called whenever a player changes there custom team selection during Game Setup 
+function NarrowMaze:OnPlayerSelectedCustomTeam(keys)
+  DebugPrint('[NARROWMAZE] OnPlayerSelectedCustomTeam')
+  DebugPrintTable(keys)
+
+  local player = PlayerResource:GetPlayer(keys.player_id)
+  local success = (keys.success == 1)
+  local team = keys.team_id
+end
+
+-- This function is called whenever an NPC reaches its goal position/target
+function NarrowMaze:OnNPCGoalReached(keys)
+  DebugPrint('[NARROWMAZE] OnNPCGoalReached')
+  DebugPrintTable(keys)
+
+  local goalEntity = EntIndexToHScript(keys.goal_entindex)
+  local nextGoalEntity = EntIndexToHScript(keys.next_goal_entindex)
+  local npc = EntIndexToHScript(keys.npc_entindex)
 end
